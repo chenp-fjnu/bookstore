11fcc2b3dbdf5165f7587b5b6cb9c6099e492bba
Ping.Chen <chp0524@gmail.com>
2024-09-20 19:31:35 +0800

    enable swagger ui

--- src/main/java/com/online/bookstore/BookStoreApplication.java
+++ src/main/java/com/online/bookstore/BookStoreApplication.java
@@ -2,9 +2,7 @@ package com.online.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
-@EnableOpenApi
 @SpringBootApplication
 public class BookStoreApplication {
     public static void main(String[] args) {
--- src/main/java/com/online/bookstore/SwaggerConfig.java
+++ src/main/java/com/online/bookstore/SwaggerConfig.java
@@ -1,22 +1,25 @@
 package com.online.bookstore;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BookStore APIs")
+                        .version("1.0")
+                        .description("API Documentation for BookStore Application")
+                        .termsOfService("https://github.com/chenp-fjnu")
+                        .contact(new Contact()
+                                        .name("Ping Chen")
+                                        .url("https://github.com/chenp-fjnu")
+                                        .email("11191162@qq.com"))
+                );
     }
 }
